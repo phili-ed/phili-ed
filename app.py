57e1c020a2c08f75f714f852ee0660ad9b4a4e61
phili-ed <phili.science@gmail.com>
2026-04-24 19:56:45 +0000

    import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import random
-from flask import Flask , request , render_template ,send_from_directory, url_for , redirect
+from flask import Flask , request , render_template ,send_from_directory, url_for , redirect , flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
